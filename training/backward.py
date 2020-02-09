c5e554b481dd7e560b570ca8417dcfc21e9ed57f
ZenMoore <1057398161@qq.com>
2020-02-09 17:38:10 +0800

    backward propagation

--- training/backward.py
+++ training/backward.py
@@ -19,6 +19,10 @@ MODEL_NAME = 'gr_model.ckpt'
 
 DATASET_PATH = '../dataset/new/'
 
+SHUFFLE_BUFFER_SIZE = 4
+
+batch_pos = 0
+
 # 将存储数据的.xls文件转为(6, 256)手势序列numpy矩阵
 def convert_to_numpy(data_file):
     book = xlrd.open_workbook(DATASET_PATH+'{}'.format(data_file))
@@ -45,10 +49,10 @@ def convert_to_numpy(data_file):
     data = np.array(list_values)
     return data
 
-# 从数据集路径中读取数据并转为两个numpy联合起来的数组作为数据集 [datas, labels]
+# 返回Dataset对象，元素为字典项 ('data': (6, 256), 'label': 1)
 def get_dataset(path):
-    datas = np.empty(shape= [288, 6, 256])
-    labels = np.empty(shape= [288])
+    datas = np.zeros(shape= [288, 6, 256])
+    labels = np.zeros(shape= [288])
     count_label = 0 # 访问到了第几个数据集子文件夹
     count_sequence = 0
     dirs = os.listdir(DATASET_PATH)
@@ -60,16 +64,49 @@ def get_dataset(path):
         for file in os.listdir(DATASET_PATH+'{}'.format(dir)):
             datas[count_sequence] = convert_to_numpy(dir+'/'+file)
             count_sequence += 1
-    return [datas, labels]
+    #todo 糟糕，label的shape错了，它应该也是嵌套矩阵，等李金旺写完代码再做修改
+    dictionary = {'data':datas, 'label':labels} #todo 预感这个字典是可能弄乱数据的祸源
+    dataset = tf.data.Dataset.from_tensor_slices(dictionary)
+    dataset.shuffle(SHUFFLE_BUFFER_SIZE) #todo 是一起打乱吗？
+    return dataset
 
-# todo 给定上一个batch的末位置加一作为所返回batch的首位置
-def next_batch(dataset, pos):
-    return np.ones(BATCH_SIZE), np.ones(BATCH_SIZE)
-
-# todo
 def train(dataset):
-    x = tf.placeholder(dtype= tf. float32, shape= [None, forward.INPUT_NODE])
-    return 0
+    iterator = dataset.make_one_shot_iterator()
+
+    data = tf.placeholder(dtype= tf.float32, shape= [None, forward.INPUT_NODE_HORIZONTAL, forward.INPUT_NODE_VERTICAL], name= 'data_input')
+    label = tf.placeholder(dtype= tf.float32, shape= [None, forward.OUTPUT_NODE], name= 'label_input')
+
+    regularizer = tf.contrib.layers.l1_regularizer(REGULARIZATION_RATE)# todo 可以修改l1范数正则为l2范数
+    y = forward.inference(data, regularizer)
+    global_step = tf.Variable(0, trainable= False)
+
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits= y, labels= tf.argmax(label, 1))
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)# todo 可以尝试其他损失函数
+    loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+
+    learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, NUM_EXAMPLES/BATCH_SIZE, LEARNING_RATE_DECAY)# todo 可以尝试更换衰减模型
+
+    variable_average = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)# todo 可以尝试更换滑动平均模型
+    variable_average_op = variable_average.apply(tf.trainable_variables())
+
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step= global_step)# todo 可以尝试更换优化算法
+    with tf.control_dependencies([train_step, variable_average_op]):
+        train_op = tf.no_op(name= 'train')
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+
+        for i in range(TRAIN_STEPS):
+            data_feed = dataset.batch()['data'] # todo 怎么分离batch后的字典项
+            label_feed = dataset.batch()['label']
+            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={data: data_feed, label: label_feed})
+
+            if i%10000 == 0:
+                print("After %d training step(s), loss on training batch is %g ." % (step, loss_value))
+                # 给出global_step参数可以让每个被保存模型的文件名末尾加上训练的轮数，比如model.ckpt=1000表示训练1000论之后的模型
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
 def main(argv = None):
     dataset = get_dataset(DATASET_PATH)
--- training/forward.py
+++ training/forward.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
 slim = tf.contrib.slim
 
-INPUT_NODE = 6 * 256
+INPUT_NODE_HORIZONTAL = 6
+INPUT_NODE_VERTICAL = 256
 OUTPUT_NODE = 8
+
+# input_tensor: (?, 6, 256)
+# output: (?,8)
+def inference(input_tensor, regularizer):
+    return [0,0]
