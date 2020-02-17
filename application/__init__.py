80fd5b335f2e2818b7ab6db74b3f380f107b10f8
ZenMoore <1057398161@qq.com>
2020-02-17 17:36:31 +0800

    application done.

--- application/__init__.py
+++ application/__init__.py
@@ -14,6 +14,16 @@ import application.send_to_demo as messenger
 #     preprocess.run(DATA_FILE)
 #
 
+model_path = "../models/gr_model.ckpt-22001"
+
+type_map = {0: 'round',
+            1: 'croix',
+            2: 'right_to_left',
+            3: 'down_to_up',
+            4: 'thunder',
+            5: 'infinity',
+            6: 'triangle',
+            7: 'turn'}
 
 # todo @author Louise: 将传入的 char[] raw 转换为 float[] 再转换为 float[6][256] 最后转换为 numpy
 # 返回 -1 表示解码失败
@@ -28,12 +38,11 @@ if __name__ == '__main__':
 
     # 数据预处理
     data = preprocess.run_without_file(data)
+
     if data == -1:
         print("decode failed.")
     else:
-        if messenger.send(inf.run(data)):
+        if messenger.send(type_map.get(inf.run(data, model_path))):
             print("Gesture type is sent to Unity3D.")
         else:
             print("Gesture type send failed.")
-
-
--- application/inference.py
+++ application/inference.py
@@ -1,8 +1,104 @@
 import tensorflow as tf
 import numpy as np
+slim = tf.contrib.slim
+
+INPUT_NODE_HORIZONTAL = 6
+INPUT_NODE_VERTICAL = 256
+OUTPUT_NODE = 8
+
+GRU_HIDDEN_SIZE = 256
+GRU_NUM_STEPS = 8
+
+input_tensor = tf.placeholder(dtype= tf.float32, shape= [None, INPUT_NODE_HORIZONTAL, INPUT_NODE_VERTICAL], name= 'data_input')
 
 
-# todo @author ZenMoore
-def run(data):
+# def get_weight_variable(shape):
+#     weights = tf.get_variable('weights', shape= shape, initializer= tf.truncated_normal_initializer(stddev= 0.1))
+#     return weights
 
-    return 'gest_type'
+
+# convert (?, 6, 256) to (?, 6, 256, 1)
+def add_one_dimension(input_tensor):
+
+    return tf.expand_dims(input_tensor, [-1])
+
+# input_tensor: (?, 6, 256)
+# output: (?,8)
+
+
+biases = tf.get_variable('biases', [48], initializer=tf.constant_initializer(0.1))
+
+input_tensor_4dims = add_one_dimension(input_tensor)
+
+with tf.variable_scope('layer_conv01'):
+    conv01_filter_weight = tf.get_variable('filter_weight', [1, 3, 1, 12], initializer= tf.truncated_normal_initializer(stddev= 0.1))
+    conv01_biases = tf.get_variable('bias', [12], initializer=tf.constant_initializer(0.0))
+    conv01 = tf.nn.conv2d(input_tensor_4dims, conv01_filter_weight, strides= [1, 1, 2, 1], padding='SAME')
+    relu01 = tf.nn.relu(tf.nn.bias_add(conv01,conv01_biases))
+
+
+# with tf.name_scope('layer_pool01'):
+#     pool01 = tf.nn.max_pool(relu01, ksize=[1, 1, 1, 1], strides=[1, 1, 1, 1], padding='VALID')
+
+with tf.variable_scope('layer_conv02'):
+    conv02_filter_weight = tf.get_variable('filter_weight', [1, 3, 12, 24], initializer= tf.truncated_normal_initializer(stddev= 0.1))
+    conv02_biases = tf.get_variable('bias', [24], initializer=tf.constant_initializer(0.0))
+    conv02 = tf.nn.conv2d(relu01, conv02_filter_weight, strides= [1, 1, 2, 1], padding='SAME')
+    relu02 = tf.nn.relu(tf.nn.bias_add(conv02,conv02_biases))
+
+# with tf.name_scope('layer_pool02'):
+#     pool02 = tf.nn.max_pool(relu02, ksize=[1, 1, 1, 1], strides=[1, 1, 1, 1], padding='VALID')
+
+with tf.variable_scope('layer_conv03'):
+    conv03_filter_weight = tf.get_variable('filter_weight', [1, 3, 24, 48], initializer= tf.truncated_normal_initializer(stddev= 0.1))
+    conv03_biases = tf.get_variable('bias', [48], initializer=tf.constant_initializer(0.0))
+    conv03 = tf.nn.conv2d(relu02, conv03_filter_weight, strides= [1, 1, 2, 1], padding='SAME')
+    relu03 = tf.nn.relu(tf.nn.bias_add(conv03,conv03_biases))
+
+# with tf.name_scope('layer_pool03'):
+#     pool03 = tf.nn.max_pool(relu03, ksize=[1, 1, 1, 1], strides=[1, 1, 1, 1], padding='VALID')
+
+with tf.variable_scope('layer_conv04'):
+    conv04_filter_weight = tf.get_variable('filter_weight', [1, 3, 48, 48], initializer= tf.truncated_normal_initializer(stddev= 0.1))
+    conv04_biases = tf.get_variable('bias', [48], initializer=tf.constant_initializer(0.0))
+    conv04 = tf.nn.conv2d(relu03, conv04_filter_weight, strides= [1, 1, 1, 1], padding='SAME')
+    relu04 = tf.nn.relu(tf.nn.bias_add(conv04,conv04_biases))
+
+# with tf.name_scope('layer_pool04'):
+#     pool04 = tf.nn.max_pool(relu04, ksize=[1, 1, 1, 1], strides=[1, 1, 1, 1], padding='VALID')
+
+with tf.variable_scope('conv_to_recur'):
+    bn = tf.layers.batch_normalization(relu04, training= False)
+    bias = tf.nn.bias_add(bn, biases) # dropout 和 bias 的顺序不知道是不是正确
+    actived = tf.nn.tanh(bias)
+
+    into_gru = tf.reduce_sum(actived, 3)
+
+with tf.variable_scope('gru_layers'):
+
+    gru = tf.nn.rnn_cell.GRUCell(GRU_HIDDEN_SIZE, activation= tf.nn.relu)
+    initial_state = gru.zero_state(batch_size= 1, dtype= tf.float32)
+    # 使用static_rnn运行num_steps, 但是不能保证遍历全部时间点，可以更换为 dynamic_rnn
+    # print(actived[:,0,:].shape)
+    # for i in range(GRU_NUM_STEPS):
+    #     if i > 0 : tf.get_variable_scope().reuse_variables()
+    #     gru_output, state = gru(into_gru[:,i,:], state= state)]
+    gru_output, state = tf.nn.dynamic_rnn(gru, into_gru, initial_state=initial_state, )
+
+with tf.variable_scope('fully_connected'):
+    fc_weight = tf.get_variable('fc_weight', [GRU_HIDDEN_SIZE, OUTPUT_NODE], initializer= tf.truncated_normal_initializer(stddev= 0.1))
+
+    fc_bias = tf.get_variable('bias', [OUTPUT_NODE], initializer= tf.constant_initializer(0.1))
+
+    logits = tf.matmul(gru_output, fc_weight) + fc_bias
+    y = tf.nn.softmax(logits=logits)
+
+
+def run(data, model_path):
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        saver.restore(sess, model_path)
+        data = [data]
+        result = np.argmax(sess.run(y, feed_dict={input_tensor: data}))
+        return result
+
